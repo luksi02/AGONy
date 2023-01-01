e5607d988ff6b0dc02c2dd83ab7a1421e8178147
luksi02 <lukaszgniewek@gmail.com>
2023-01-01 23:28:33 +0100

    2023_01_01_LG1

--- gra/settings.py
+++ gra/settings.py
@@ -16,6 +16,11 @@ from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = Path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "/media/"
+
+#MEDIA_ROOT = 'home/luksi02/AGONy/AGONy/monsters_images'
 
 
 # Quick-start development settings - unsuitable for production
