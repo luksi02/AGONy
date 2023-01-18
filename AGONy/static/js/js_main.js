3398a82667235b3ea5a0b88e2db2ddb34210e2d4
luksi02 <lukaszgniewek@gmail.com>
2023-01-18 22:01:13 +0100

    2023_01_18_LG1

--- AGONy/static/js/js_main.js
+++ AGONy/static/js/js_main.js
@@ -5,6 +5,7 @@ const clickedButtons = document.querySelectorAll(".btn")
 clickedButtons.forEach(function (button) {
     button.addEventListener('click', function (event) {
         console.log('button was clicked!')
+
     //     rest of the magic should happen here!
     })
 })
